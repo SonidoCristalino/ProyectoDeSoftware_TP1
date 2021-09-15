68941527553fc4b593c26d25a21404c0eeb55e5d
emiliano.salvatori <emiliano.salvatori@globallogic.com>
2021-09-15 02:43:08 -0300

    Se agrega la tabla a Domain.Entities

---
+++ TrabajoPractico1.Domain/Entities/Tickets.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabajoPractico1.Domain.Entities
+{
+    public class Tickets
+    {
+        public Guid TicketId { get; set; }
+        public int FuncionId { get; set; }
+        public string Usuario { get; set; }
+     
+    }
+}
