d950775dcaa9c19101dd390182f07993c2850eed
emiliano.salvatori <emiliano.salvatori@globallogic.com>
2021-09-15 02:44:22 -0300

    Se agrega la clase Salas a Domain.Entities

---
+++ TrabajoPractico1.Domain/Entities/Salas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabajoPractico1.Domain.Entities
+{
+    public class Salas
+    {
+        public int SalaId { get; set; }
+        public int Capacidad { get; set; }
+        public IList<Funciones> FuncionesNavigator { get; set; }
+    }
+}
